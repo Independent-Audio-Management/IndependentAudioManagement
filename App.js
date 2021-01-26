c5cdbc18a50d74ae3f62d690e91b4bcce58064ca
Tasmiha Hassan <htasmiha@gmail.com>
2021-01-26 01:04:58 -0500

    Some bugs still occus

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import store from "./src/app/store";
 import { Provider } from "react-redux";
+import store from "./src/app/store";
 import AppStack from "./src/navigations/AppStack";
 export default function App() {
   return (
