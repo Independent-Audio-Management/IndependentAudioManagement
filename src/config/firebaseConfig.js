d73b8a50f95459edf191d59966431b1b9b141ea1
Tasmiha Hassan <htasmiha@gmail.com>
2021-02-09 11:24:57 -0500

    Fixed production build

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,17 +1,20 @@
-import * as firebase from 'firebase';
-import getEnvVars from '../../environment';
-const { apiKey, authDomain, databaseURL, projectId, storageBucket } = getEnvVars();
+import * as firebase from "firebase";
+// const { apiKey, authDomain, databaseURL, projectId, storageBucket } = getEnvVars();
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: `${apiKey}`,
-  authDomain: `${authDomain}`,
-  databaseURL: `${databaseURL}`,
-  projectId: `${projectId}`,
-  storageBucket: `${storageBucket}`,
+  // apiKey: `${apiKey}`,
+  // authDomain: `${authDomain}`,
+  // databaseURL: `${databaseURL}`,
+  // projectId: `${projectId}`,
+  // storageBucket: `${storageBucket}`,
+  apiKey: "mAIzaSyDvYXRpVWA_4S39bJLocayCWDRAy738LO8",
+  authDomain: "i-am-68785.firebaseapp.com",
+  databaseURL: "https://i-am-68785.firebaseio.com",
+  projectId: "i-am-68785",
+  storageBucket: "i-am-68785.appspot.com",
   messagingSenderId: "sender-id",
   appId: "app-id",
-  measurementId: "G-measurement-id"
+  measurementId: "G-measurement-id",
 };
 
-if (!firebase.apps.length)
-    firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) firebase.initializeApp(firebaseConfig);
